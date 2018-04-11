7d8b32001461cf94bc489c11e8b94e324fa7ff04
camneu37 <neuner.camille@gmail.com>
2018-04-10 23:14:55 -0700

    update books new view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,23 @@
 class BooksController < ApplicationController
 
   get '/books' do
-    @books = Book.all
-    erb :'/books/index'
+    if logged_in?
+      @books = Book.all
+      erb :'books/index'
+    else
+      redirect '/'
+    end
   end
+
+  get '/books/new' do
+    if logged_in?
+      erb :'books/new'
+    else
+      redirect '/'
+    end
+  end
+
+  ##DO POST BOOKS ROUTE NEXT TO HANDLE CREATING A NEW BOOK THE USER ADDED!
+  #Need google how to make validations for user input to ensure that bad data (aka duplicate data) isn't added. Is there a smoother way than doing find_by and each of the attributes before creating?
 
 end
