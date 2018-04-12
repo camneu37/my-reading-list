6e527005528159c0a18f7788c2e375a1944716d5
camneu37 <neuner.camille@gmail.com>
2018-04-11 21:10:41 -0700

    add get books/:slug action to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,14 @@ class BooksController < ApplicationController
     redirect "/books/#{book.slug}"
   end
 
+  get '/books/:slug' do
+    if logged_in?
+      erb :'books/show'
+    else
+      redirect '/'
+    end
+  end
+
 
   ##DO POST BOOKS ROUTE NEXT TO HANDLE CREATING A NEW BOOK THE USER ADDED!
   #Need google how to make validations for user input to ensure that bad data (aka duplicate data) isn't added. Is there a smoother way than doing find_by and each of the attributes before creating?
