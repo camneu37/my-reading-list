a1f9283289de5d49086ea752e7c3e155bc6ffb5d
camneu37 <neuner.camille@gmail.com>
2018-04-16 21:32:21 -0700

    add comments above methods in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < Sinatra::Base
     use Rack::Flash
   end
 
+  #shows the landing page, promps user to log in or sign up
   get '/' do
     if !logged_in?
       erb :index
@@ -17,11 +18,14 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+
   helpers do
+    #checks if there is a user logged in
     def logged_in?
       !!session[:user_id]
     end
 
+    #checks to see who the current user is
     def current_user
       User.find_by_id(session[:user_id])
     end
