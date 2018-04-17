1f58a34522e89fd615a59bf1d08dae2ac6ea70ee
camneu37 <neuner.camille@gmail.com>
2018-04-16 21:34:06 -0700

    add comments above methods in authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
 
+  #renders the list of authors that exist in the app, if a user is logged in, otherwise, redirect user to site landing page
   get '/authors' do
     if logged_in?
       @authors = Author.all
@@ -9,6 +10,7 @@ class AuthorsController < ApplicationController
     end
   end
 
+  #renders the page for an individual author if a user is logged in, otherwise, redirect user to site landing page
   get '/authors/:slug' do
     if logged_in?
       @author = Author.find_by_slug(params[:slug])
