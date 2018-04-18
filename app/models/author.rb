0049a68e62f583a39cff4527c1d3860b9ef4a019
camneu37 <neuner.camille@gmail.com>
2018-04-17 17:36:51 -0700

    update slugifiable module to only include the class method; add slug method to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,5 +4,9 @@ class Author < ActiveRecord::Base
   has_many :books
 
   extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
+
+  def slug
+    self.name.downcase.gsub(/[^a-z1-9]+/, '-').chomp('-')
+  end
+
 end
--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,14 +1,9 @@
 module Slugifiable
-  module InstanceMethods
-
-    def slug
-      self.name.downcase.gsub(/[^a-z1-9]+/, '-').chomp('-')
-    end
-  end
 
   module ClassMethods
     def find_by_slug(slug)
       self.all.find{|i| i.slug == slug}
     end
   end
+  
 end
